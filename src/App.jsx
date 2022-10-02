c7c2215a08d2f1e5eb1f1c580e8c8b2ffa33d2c9
Kishan Mandora <kishanmandora7@gmail.com>
2022-10-02 19:46:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2> Yt-Dēta </h2>
+      <p> Your goto app for extracting the data of Youtube Videos </p>
+    </div>
+  );
+}
+
+export default App;
