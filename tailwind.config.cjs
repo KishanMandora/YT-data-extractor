04502fd40082f90446836e0fb5edb2533790f634
Kishan Mandora <kishanmandora7@gmail.com>
2022-10-04 18:31:58 +0530

    chore: add tailwind prettier plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kanit: ["Kanit"],
+        dirt: ["Rubik Dirt"],
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
