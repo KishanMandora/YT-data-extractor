3791cfdc9e303a79397ca3660046e47bf64e39c5
Kishan Mandora <kishanmandora7@gmail.com>
2022-10-06 16:23:00 +0530

    feat: add delete data feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,24 @@
-import axios from "axios";
 import { useState } from "react";
 import { Header } from "./Components/Header";
 import { Form } from "./Components/Form";
 import { DisplayData } from "./Components/DisplayData";
-import { getUrl } from "./helpers/getUrl";
 import "./App.css";
 import { Toast } from "./Components/Toast";
 import { useLocalStorage } from "./Hooks/useLocalStorage";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
   const [loader, setLoader] = useState(false);
   const [data, setData] = useLocalStorage("data", []);
   const [error, setError] = useState(null);
-
-  const submitHandler = async (e) => {
-    e.preventDefault();
-
-    const currentUrl = getUrl(inputValue);
-
-    setLoader(true);
-    try {
-      // const sleep = await new Promise((r) => setTimeout(r, 1300));
-      const { data: responseData } = await axios.get(currentUrl);
-
-      if (responseData.items.length) {
-        setData((data) => [...data, responseData]);
-        setError(null);
-        setLoader(false);
-        setInputValue("");
-      } else {
-        setLoader(false);
-        setInputValue("");
-        setError({ msg: `Please enter a valid URL`, type: "error" });
-        const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
-        await sleep(3000);
-        setError(null);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <div className="App font-kanit">
       <Header />
       <Form
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-        submitHandler={submitHandler}
+        setLoader={setLoader}
+        setData={setData}
+        setError={setError}
+        data={data}
       />
       {loader && <h3> LOADING..... </h3>}
       {error && <Toast msg={error.msg} type={error.type} />}
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,4 +1,40 @@
-function Form({ submitHandler, inputValue, setInputValue }) {
+import axios from "axios";
+import { useState } from "react";
+import { getUrl } from "../helpers/getUrl";
+
+function Form({ setData, setError, setLoader, data }) {
+  const [inputValue, setInputValue] = useState("");
+
+  console.log(data.length);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const currentUrl = getUrl(inputValue);
+
+    setLoader(true);
+    try {
+      // const sleep = await new Promise((r) => setTimeout(r, 1300));
+      const { data: responseData } = await axios.get(currentUrl);
+
+      if (responseData.items.length) {
+        setData((data) => [...data, responseData]);
+        setError(null);
+        setLoader(false);
+        setInputValue("");
+      } else {
+        setLoader(false);
+        setInputValue("");
+        setError({ msg: `Please enter a valid URL`, type: "error" });
+        const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+        await sleep(3000);
+        setError(null);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const isDisabled = () => {
     const regex = new RegExp(
       "^(http(s)?://)?((w){3}.)?youtu(be|.be)?(.com)?/.+"
@@ -24,6 +60,14 @@ function Form({ submitHandler, inputValue, setInputValue }) {
           disabled={isDisabled()}
         >
           Show Data
+        </button>
+        <button
+          className="btn btn-error rounded-sm"
+          type="reset"
+          onClick={() => setData([])}
+          disabled={data.length ? false : true}
+        >
+          Delete Data
         </button>
       </form>
     </div>
