aedd2307074fe0e745e10254a3b38d6c2f329e99
Kishan Mandora <kishanmandora7@gmail.com>
2022-10-06 16:37:10 +0530

    feat: add multiple links support

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -5,43 +5,49 @@ import { getUrl } from "../helpers/getUrl";
 function Form({ setData, setError, setLoader, data }) {
   const [inputValue, setInputValue] = useState("");
 
-  console.log(data.length);
-
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    const currentUrl = getUrl(inputValue);
+    const urls = inputValue.split(",");
 
-    setLoader(true);
-    try {
-      // const sleep = await new Promise((r) => setTimeout(r, 1300));
-      const { data: responseData } = await axios.get(currentUrl);
+    console.log(urls);
 
-      if (responseData.items.length) {
-        setData((data) => [...data, responseData]);
-        setError(null);
-        setLoader(false);
-        setInputValue("");
-      } else {
-        setLoader(false);
-        setInputValue("");
-        setError({ msg: `Please enter a valid URL`, type: "error" });
-        const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
-        await sleep(3000);
-        setError(null);
+    for (let i = 0; i < urls.length; i++) {
+      const currentUrl = getUrl(urls[i].trim());
+      setLoader(true);
+      try {
+        // const sleep = await new Promise((r) => setTimeout(r, 1300));
+        const { data: responseData } = await axios.get(currentUrl);
+        if (responseData.items.length) {
+          setData((data) => [...data, responseData]);
+          setError(null);
+          setLoader(false);
+          setInputValue("");
+        } else {
+          setLoader(false);
+          setInputValue("");
+          setError({ msg: `${urls[i]} is invalid URL`, type: "error" });
+          const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+          await sleep(3000);
+          setError(null);
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
     }
   };
 
-  const isDisabled = () => {
-    const regex = new RegExp(
-      "^(http(s)?://)?((w){3}.)?youtu(be|.be)?(.com)?/.+"
-    );
+  // const isDisabled = () => {
+  //   const regex = new RegExp(
+  //     "^(http(s)?://)?((w){3}.)?youtu(be|.be)?(.com)?/.+"
+  //   );
 
-    const isValidUrl = regex.test(inputValue);
-    return isValidUrl ? false : true;
+  //   const isValidUrl = regex.test(inputValue);
+  //   return isValidUrl ? false : true;
+  // };
+
+  const inputChangeHandler = (e) => {
+    setInputValue(e.target.value);
   };
 
   return (
@@ -52,12 +58,12 @@ function Form({ setData, setError, setLoader, data }) {
           className="input input-bordered input-primary w-full max-w-xs rounded-r-none"
           type="text"
           value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+          onChange={(e) => inputChangeHandler(e)}
         />
         <button
           className="btn btn-primary rounded-l-none"
           type="submit"
-          disabled={isDisabled()}
+          // disabled={isDisabled()}
         >
           Show Data
         </button>
